0d34b68aa8b8a044e92a20dac73d27afc7b659ba
Easyliving <118458340+connellYo@users.noreply.github.com>
2023-12-13 00:31:32 +0900

    싱글턴

---
+++ src/main/java/singleton/Tab.java
@@ -0,0 +1,10 @@
+package singleton;
+
+public class Tab {
+    //  ⭐️ 공유되는 유일한 인스턴스를 받아옴
+    private Setting setting = Setting.getInstance();
+
+    public Setting getSetting() {
+        return setting;
+    }
+}
